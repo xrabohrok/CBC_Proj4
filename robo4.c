38c54c0960c0a83b4584b528b6fd77f29673c6db
unknown <xrabohrok>
2013-04-10 21:56:58 -0500

    rotation code is accurate, but does not stop at specified facing

--- robo4.c
+++ robo4.c
@@ -25,7 +25,8 @@ const int branch[3][5][2] =
 };
 
 const float wheelRad = .03f; //meters
-const float axelLength = .15;
+const float axelLength = .15f;
+const float PI = 3.142f;
 
 const float cellSize = .3f; //meters
 
@@ -35,22 +36,34 @@ const int SPEED = 300;
 const int MIN_OBJECT_SIZE  = 10;
 const int SCREEN_WIDTH = 159;
 
+const float ROT_THRESH = .06f;
+
 #include <stdio.h>
 
 void selectColor();
 void moveTowardFoundObject();
 void missingObjects();
+void rotateTo(int x, int y);
+void accumulateMoveData();
+
+
 int color = 0;
 int objectFound = 0;
 
 
+int cellX;
+int cellY;
 float posX; //meters
 float posY; //meters
+float rot;  //rad
 
 int main() 
 {
 	posX = 0;
 	posY = 0;
+	rot = PI;
+	cellX = 5;
+	cellY = 9;
 	
 	printf("Hello, World!\n");
 	set_each_analog_state(1,1,1,0,0,0,0,0);
@@ -60,7 +73,7 @@ int main()
     while(1)
     {
         //DO I SEE ANYTHING
-    
+		rotateTo(9,9);
     
         //avoid any detected obstacles
         
@@ -70,6 +83,7 @@ int main()
         
         //data stuff
         accumulateMoveData();
+		sleep(.5f);
     }
 	
 	return 0;
@@ -112,32 +126,46 @@ void accumulateMoveData()
 	float distL;
 	float distR;
 	float theta;
-	float xDiff
 	float r;
+	float xDiff;
 	float xDisp;
 	float xR;
 	
-	ticksR = get_motor_position_counter(1); //500
+	ticksR = get_motor_position_counter(3); //500
 	ticksL = get_motor_position_counter(0); //350
 	
-	distR = ticksR/ticksPerRev * wheelRad; //.135
-	distL = ticksL/ticksPerRev * wheelRad; //.0095454
+	distR = (float)(ticksR)/ticksPerRev * wheelRad * PI ; //.135
+	distL = (float)(ticksL)/ticksPerRev * wheelRad * PI ; //.0095454
 	
-	theta = (distR-distL)/axelLength; //.8364
+	theta = (distR-distL)/(axelLength/2); //.8364
+	//printf("distances l %f r %f", distL, distR);
 	
     if (theta > 0)
         r = distR/theta; 
     else
         r = distL/theta;
 	
-	xdisp = (posX - r - axelLength/2);
-	xR = posX - xdisp;
-	posX = xR * cos(theta)  + xdisp; //- 0 * sin(theta);
-	posY = xR * sin(theta) + posY; //- 0 * sin(theta);
+	xDisp = (posX - r - axelLength/2);
+	xR = posX - xDisp;
+	//make sure not spinning in place
+	if(!((distL > 0 && distR < 0) || (distL < 0 && distR > 0)))
+	{
+		posX = xR * cos(theta)  + xDisp;//- 0 * sin(theta);
+		posY = xR * sin(theta) + posY; //- 0 * sin(theta);
+	}
+	
+	rot += theta;
 	
 	
+	while (rot < 0)
+		rot += 2 *PI;
 	
 	
+	cellX = posX / cellSize + 5;
+	cellY = posY / cellSize + 9;
+	
+	clear_motor_position_counter(0);
+	clear_motor_position_counter(3);
 	//clear_motor_position_counter(motor)
 	//get_motor_position_counter(motor)
 }
@@ -145,6 +173,56 @@ void accumulateMoveData()
 //rotates to face cell provided
 void rotateTo(int x, int y)
 {
+	float diffX = (x - cellX) * cellSize;
+	float diffY = (y - cellY) * cellSize;
+	
+	float destAngle = atan(diffY/diffX);
+	//since there is not tan2...
+	//quad1
+	if(diffX > 0 && diffY > 0)
+	{
+		//if(destAngle < 0)
+		//	destAngle = -1;
+	}
+	//quad2
+	else if(diffX > 0 && diffY < 0)
+	{
+		if(destAngle < 0)
+			destAngle += PI/2;
+	}
+	//quad3
+	else if(diffX < 0 && diffY < 0)
+	{
+		if(destAngle > 0)
+			destAngle += PI;
+	}
+	//quad4
+	else if(diffX > 0 && diffY > 0)
+	{
+		if(destAngle < 0)
+			destAngle += PI * 1.5;
+	}
+	
+	while (destAngle >= PI * 2)
+		destAngle -= PI * 2;
+
+	
+	
+	printf ("dest:%f, rot:%f\n", destAngle, rot);
+	if (rot > destAngle + ROT_THRESH)
+	{
+		mav(0,SPEED/2);
+		mav(3,-SPEED/2);
+	}
+	else if (rot < destAngle - ROT_THRESH)
+	{
+		mav(0,-SPEED/2);
+		mav(3,SPEED/2);
+	}
+	else
+	{
+		ao();
+	}	
 }
 
 void evasiveManuver(int ticks)
