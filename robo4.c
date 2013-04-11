52a1a6bf8720780ad113b564f940a32a63e683b2
xrabohrok <xrabohrok>
2013-04-11 06:50:28 -0500

    finished, basically

--- robo4.c
+++ robo4.c
@@ -20,8 +20,8 @@ const int map[11][11] =
 const int branch[3][5][2] = 
 {
 {{9,5},{9,8},{7,8},{-1,-1},{-1,-1}},
-{{9,5},{5,5},{2,5},{2,8},{3,8}},
-{{9,5},{9,2},{3,1},{3,2},{-1,-1}}
+{{9,5},{5,5},{2,8},{2,8},{3,8}},
+{{9,2},{7,1},{3,1},{3,2},{-1,-1}}
 };
 
 const float wheelRad = .03f; //meters
@@ -30,13 +30,16 @@ const float PI = 3.1415926f;
 
 const float cellSize = .3f; //meters
 
-const int ticksPerRev = 1025;
+const int ticksPerRev = 1060;
 
 const int SPEED = 300;
 const int MIN_OBJECT_SIZE  = 10;
 const int SCREEN_WIDTH = 159;
 
+const int TICK_SHAVE = 30; //subtracts these ticks from rotation, because 
+
 const float ROT_THRESH = .2f;
+
 
 #include <stdio.h>
 
@@ -45,7 +48,7 @@ void moveTowardFoundObject();
 void missingObjects();
 void Rotate90();
 void moveSquare();
-
+void accumulateMoveData();
 
 int color = 0;
 int objectFound = 0;
@@ -57,7 +60,6 @@ float posX; //meters
 float posY; //meters
 float rot;  //rad
 
-
 int main() 
 {
 	int goal = 0; //to 2
@@ -65,6 +67,7 @@ int main()
 	int link = 0; //of nodes of chain
 	int direction = 1; //1 = forward, -1 = backward
 	int facing = 0; //where 0 is facing down the hallway(negative) and advances ccw to 3
+	int triggered = 0;
 	
 	posX = 0;
 	posY = 0;
@@ -72,94 +75,95 @@ int main()
 	cellX = 5;
 	cellY = 9;
 	
-	printf("Hello, World!\n");
 	set_each_analog_state(1,1,1,0,0,0,0,0);
 	selectColor();
     //main loop
-    while(1)
-    {
-        //DO I SEE ANYTHING
-		
-		//look at goal, am I there
+    while(1)    
+	{ 
+		//DO I SEE ANYTHING
+		//look at goal, am I there	
 		//yes
-		if(branch[goal][link][0] == cellY && branch[goal][link][1] == cellX)
+		track_update();
+		int size = track_size(color,0);
+		if (track_count(color) > 0 && size >= MIN_OBJECT_SIZE)
 		{
-			//advance goal chain
-			link += direction;
-			//overlimit
-			if(link >= 5 || link < 0)
+			triggered = 1;
+		}
+		if(triggered == 0)
+		{
+			if(branch[goal][link][0] == cellY && branch[goal][link][1] == cellX )
 			{
-				if (link < 0)
+				//advance goal chain
+				link += direction;	
+				//overlimit	
+				if(link >= 5 || link < 0)
 				{
-					//got to beginning, send us on a new adventure
-					goal++;
-					if (goal > 2)
-						goal = 0;
+					if (link < 0)	
+					{	
+						//got to beginning, send us on a new adventure
+						goal++;	
+						if (goal > 2)	
+							goal = 0;
+					}
+					direction *= -1;	
+					link += 2 * direction;
 				}
-				direction *= -1;
-				link += 2 * direction;
-				
+				else if(branch[goal][link][0] == -1)	
+				{
+					direction *= -1;
+					link += 2 * direction;
+				}
+				printf("new Destination: %d, %d", branch[goal][link][0], branch[goal][link][1]);
 			}
-			else if(branch[goal][link][0] == -1)
+			//am I facing goal?	
+			//needs to be...(x resolved first, unless map says no)
+			printf("\t\t%d, %d", cellY, cellX);
+			if(cellX > branch[goal][link][1] && map[cellY][cellX-1] == 0)
 			{
-				direction *= -1;
-				link += 2 * direction;
-				
+				goalDir = 1;
+				printf("west\n");
+			}	
+			else if(cellX < branch[goal][link][1] && map[cellY][cellX+1] == 0)
+			{
+				goalDir = 3;	
+				printf("east\n");	
 			}
-			printf("new Destination: %d, %d", branch[goal][link][0], branch[goal][link][1]);
+			else if(cellY > branch[goal][link][0] && map[cellY-1][cellX] == 0)
+			{	
+				goalDir = 0;
+				printf("north\n");	
+			}
+			else if(cellY < branch[goal][link][0] && map[cellY+1][cellX] == 0)
+			{	
+				goalDir = 2;	
+				printf("south\n");
+			}	
+			//if not right facing, resolve now
+			while(facing != goalDir)
+			{	
+				printf("%d vs %d", facing, goalDir);
+				Rotate90();	
+				facing++;
+				if(facing > 3)	
+					facing = 0;	
+			}
+			moveSquare();
+			//move forward	
+			if(facing == 0)	
+				cellY--;
+			else if (facing == 2)
+				cellY++;
+			else if(facing == 1)
+				cellX--;
+			else if (facing == 3)	
+				cellX++;        		//sleep(.5f);
 		}
-		
-		//am I facing goal?
-		//needs to be...(x resolved first, unless map says no)
-		printf("\t\t%d, %d", cellY, cellX);
-		if(cellX > branch[goal][link][1] && map[cellY][cellX-1] == 0)
+		else
 		{
-			goalDir = 1;
-			printf("west\n");
-		}
-		else if(cellX < branch[goal][link][1] && map[cellY][cellX+1] == 0)
-		{
-			goalDir = 3;
-			printf("east\n");
-		}
-		else if(cellY > branch[goal][link][0] && map[cellY-1][cellX] == 0)
-		{
-			goalDir = 0;
-			printf("north\n");
-		}
-		else if(cellY < branch[goal][link][0] && map[cellY+1][cellX] == 0)
-		{
-			goalDir = 2;
-			printf("south\n");
+			moveTowardFoundObject();
 		}
 		
-		//if not right facing, resolve now
-		while(facing != goalDir)
-		{
-			printf("%d vs %d", facing, goalDir);
-			Rotate90();
-			facing++;
-			if(facing > 3)
-				facing = 0;
-		}
-		
-		
-		
-		moveSquare();
-		
-		//move forward
-		if(facing == 0)
-			cellY--;
-		else if (facing == 2)
-			cellY++;
-		else if(facing == 1)
-			cellX--;
-		else if (facing == 3)
-			cellX++;
-
-        
-		//sleep(.5f);
-    }
+	}
 	
 	return 0;
 }
@@ -192,7 +196,55 @@ void selectColor(){
 	sleep(2.0);
 	
 }
-
+//determines location and cell from that info
+void accumulateMoveData()
+{
+	//lord help me if this math is wrong
+	int ticksR; 
+	int ticksL;
+	float distL;
+	float distR;
+	float theta;
+	float r;
+	float xDiff;
+	float xDisp;
+	float xR;
+	float d;
+	
+	ticksR = get_motor_position_counter(3); //500
+	ticksL = get_motor_position_counter(0); //350
+	
+	distR = ((float)(ticksR))/((float)ticksPerRev) * wheelRad * PI *2; //.135
+	distL = ((float)(ticksL))/((float)ticksPerRev) * wheelRad * PI *2; //.0095454
+	
+	
+	theta = ((distL-distR)/2)/(((float)axelLength)/2); //.8364
+	//theta = ((float)(ticksR - ticksL))/((float)(ticksPerRev)) * 2 * PI;
+	//printf("ticks %d %d distances l %f r %f t%f",ticksR, ticksL, distL, distR, theta);
+	
+    if (theta > 0)
+        r = distR/theta; 
+    else
+        r = distL/theta;
+	
+	d = theta *(r + axelLength/2) ;
+	
+	rot += theta;
+	
+	while (rot < 0)
+		rot += 2 *PI;
+	
+	posX = cos(rot) * rot;
+	posY = sin(rot) * rot;
+	
+	cellX = posX / cellSize + 5;
+	cellY = posY / cellSize + 9;
+	
+	clear_motor_position_counter(0);
+	clear_motor_position_counter(3);
+	//clear_motor_position_counter(motor)
+	//get_motor_position_counter(motor)
+}
 
 //rotates to face cell provided
 void Rotate90()
@@ -207,7 +259,7 @@ void Rotate90()
 	
 	quarterTurn = .25f * (2 * PI * axelLength/2);
 	revs = quarterTurn / (2 * PI * wheelRad);
-	ticksNeeded = ticksPerRev * revs;
+	ticksNeeded = ticksPerRev * revs -TICK_SHAVE;
 	
 	//printf("needtorotate %d", ticksNeeded);
 	
@@ -216,8 +268,9 @@ void Rotate90()
 	
 	while((get_motor_done(0)==0))
 	{
-		//sleep(.5);
+		sleep(.5);
 	}
+	
 	
 }
 
@@ -235,7 +288,7 @@ void moveSquare()
 	//printf("done? %d", get_motor_done(0));
 	while((get_motor_done(0)==0))
 	{
-		//sleep(.5);
+		sleep(.5);
 	}
 }
 
